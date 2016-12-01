e022e7148fb2ed085bca51967185d28187df0682
SanyaZoom <alexandrsemenets1991@gmail.com>
2016-12-01 23:06:18 +0200

    Add files via upload

---
+++ src/server/db/repos/bssaldocard.ts
@@ -0,0 +1,31 @@
+import {IDatabase, IMain} from 'pg-promise';
+import sqlProvider = require('../sql');
+
+var sql = sqlProvider.bssaldocard;
+
+/*
+ This repository mixes hard-coded and dynamic SQL,
+ primarily to show a diverse example of using both.
+ */
+
+export class Repository {
+
+    constructor(db:any, pgp:IMain) {
+        this.db = db;
+        this.pgp = pgp;
+    }
+
+    // if you need to access other repositories from here,
+    // you will have to replace 'IDatabase<any>' with 'any':
+    private db:IDatabase<any>;
+
+    private pgp:IMain;
+
+    // Creates the table;
+    create = () =>
+        this.db.none(sql.create);
+
+    // Returns all user records;
+    all = () =>
+        this.db.any('SELECT * FROM bs.bssaldocard');
+}
